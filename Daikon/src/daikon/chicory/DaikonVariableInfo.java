335c87925502b0c155027003d4435314c162e968
Ziyad Alsaeed <zalsaeed@cs.uoregon.edu>
2017-11-29 16:48:09 -0800

    more decribtive messages.

--- Daikon/src/daikon/chicory/DaikonVariableInfo.java
+++ Daikon/src/daikon/chicory/DaikonVariableInfo.java
@@ -1332,7 +1332,7 @@ public abstract class DaikonVariableInfo
 	   //return immediately if the list is still not initialized.
 	   if(listInstance == null) {
 	     if(Runtime.working_debug)
-           System.out.println("\t\t\t\t\t[Chicory.DaikonVariableInfo.getListElements()] List is not initialized, abort!");
+           System.out.println("\t\t\t\t\t[Chicory.DaikonVariableInfo.getListElements()] List is not initialized, abort!" + listInstance.getClass().getName());
          return listOfElements;
        }
 	   
